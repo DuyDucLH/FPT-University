96301aac5fe35d47c2ae916ad66d7b5007ba1649
DuyDucLH <duylhdhe172114@fpt.edu.vn>
2023-02-03 14:23:15 +0700

    update CSD201_LinkedListExercise_HE172114

---
+++ 3rd semester/CSD201/CSD201_LinkedListExercise_HE172114/src/Person.java
@@ -0,0 +1,18 @@
+
+public class Person {
+
+    String name;
+    int age;
+
+    Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return "(" + name + ", " + age + ") ";
+    }
+}
